ad70631ea766108ddc30b2c2e6f5115aa44429e3
NaziraDi <diushebekova@gmail.com>
2022-11-07 23:36:32 +0600

    method is renamed

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,16 +1,18 @@
 package lang.print.gaps.task2;
 
 public class AdvancedNamingConvention {
+
+
     private static final int ADULT_AGE = 18;
 
     private int age;
     private int phoneNumber;
 
-    void callToFriend() {
+    void callToFriend(int phoneNumber) {
         //some code
     }
 
-    void callByNumber(int number) {
+    void callByNumber(int phoneNumber) {
         //some code
     }
 }
